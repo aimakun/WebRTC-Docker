0a650a7874c308688e6a72a63a6bf7a25027e948
aimakun <aima.kun@gmail.com>
2019-06-30 18:04:05 +0700

    Add setup script for native (not docker)

---
+++ apprtc-server/setup-native.sh
@@ -0,0 +1,71 @@
+DEBIAN_FRONTEND=noninteractive apt-get update -y &&     apt-get install -y     build-essential     apt-transport-https ca-certificates curl software-properties-common     supervisor
+adduser --disabled-password --gecos "" deploy
+su deploy -c     "bash \"install -m700 -d /home/deploy/.ssh\""
+sudo su deploy
+fallocate -l 2G /swapfile && chmod 600 /swapfile && mkswap /swapfile
+swapon /swapfile
+swapon --show
+cp /etc/fstab /etc/fstab.bak
+echo "/swapfile none swap sw 0 0" >> /etc/fstab
+sysctl vm.swappiness=10
+sysctl vm.vfs_cache_pressure=50
+echo "vm.swappiness=10\nvm.vfs_cache_pressure = 50" >> /etc/sysctl.conf
+swapon --show
+echo "LC_ALL=en_US.UTF-8" >> /etc/environment
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+locale-gen en_US.UTF-8
+update-locale
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && apt-key fingerprint 0EBFCD88
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+DEBIAN_FRONTEND=noninteractive apt-get update -y && apt-get install docker-ce -y
+usermod -aG docker ubuntu
+sudo -H -u deploy bash -c "curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash"
+sudo -H -u deploy bash -c ". ~/.nvm/nvm.sh; nvm install --lts; npm install -g gulp-cli"
+export GAE_VER=1.9.74
+export GOLANG_VER=1.8.3
+export GOLANG_TAR=go$GOLANG_VER.linux-amd64.tar.gz
+wget https://storage.googleapis.com/golang/$GOLANG_TAR
+tar -C /usr/local -xzf $GOLANG_TAR
+export PATH=$PATH:/usr/local/go/bin
+export GOPATH=/goWorkspace
+mkdir -p $GOPATH/src
+
+export LIBEVENT_VER=2.1.8
+export COTURN_VER=4.5.0.7
+export PUBLIC_IP=127.0.0.1
+
+wget https://github.com/libevent/libevent/releases/download/release-$LIBEVENT_VER-stable/libevent-$LIBEVENT_VER-stable.tar.gz
+tar xvfz libevent-$LIBEVENT_VER-stable.tar.gz
+cd libevent-$LIBEVENT_VER-stable
+./configure && make && make install
+
+cd /home/ubuntu/
+wget http://turnserver.open-sys.org/downloads/v$COTURN_VER/turnserver-$COTURN_VER.tar.gz
+tar xvfz turnserver-$COTURN_VER.tar.gz
+cd turnserver-$COTURN_VER
+./configure && make && make install
+turnadmin -a -u ninefingers -r apprtc -p youhavetoberealistic
+
+vim /etc/turnserver.conf
+
+cd /home/ubuntu/
+git clone https://github.com/aimakun/apprtc.git
+cd apprtc
+git checkout dev-appscale
+ln -s /home/ubuntu/apprtc/src/collider/collider $GOPATH/src
+ln -s /home/ubuntu/apprtc/src/collider/collidermain $GOPATH/src
+ln -s /home/ubuntu/apprtc/src/collider/collidertest $GOPATH/src
+
+go get collidermain
+go install collidermain
+
+# Use WebRTC-Docker for supervisord config & ice.js
+cd /home/ubuntu/
+git clone https://github.com/aimakun/WebRTC-Docker.git
+cd WebRTC-Docker/apprtc-server/
+sudo -H -u deploy bash -c ". ~/.nvm/nvm.sh; nvm install 8; nvm use 8; cd /home/ubuntu/WebRTC-Docker/apprtc-server/ && npm install express cors"
+mkdir /webrtc_avconf && chmod 777 /webrtc_avconf
+cp turnserver.conf /etc/turnserver.conf
+cp ice.js /ice.js
+supervisord -c /home/ubuntu/WebRTC-Docker/apprtc-server/apprtc_supervisord.conf
